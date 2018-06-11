c28ab994c7d5be47abdf560b0e15e4e58b13d2a1
Jay Hayes <ur@iamvery.com>
2018-06-11 11:48:14 -0500

    wip [ci skip]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :lolwat
+
+  def index
+    render plain: 'lolwat'
+  end
+
+  private
+
+  alias_method :haha, :lolwat
+  def lolwat
+    puts 'HAHAHA'
+  end
 end
